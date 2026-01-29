70d7a844bb669e669306353e5be7fd7aa2ca1221
mamat hensem <hensem@gmail.com>
2026-01-29 15:49:05 +0800

    remove controller.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-require_once 'controller.php';
+require_once 'init.php';
+
+// Prepare template variables
+$template_vars = [
+    'login_button' => $login_button,
+    'user_image' => $_SESSION['user_image'] ?? '',
+    'user_name' => ($_SESSION['user_first_name'] ?? '') . ' ' . ($_SESSION['user_last_name'] ?? ''),
+    'user_email' => $_SESSION['user_email_address'] ?? '',
+    'csrf_token' => $_SESSION['csrf'] ?? '',
+    'contributor' => $contributor ?? [],
+    'current_year' => date('Y')
+];
+
+// Include the template
+include 'template.php';
 ?>
